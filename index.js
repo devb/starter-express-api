804e8b4c57464b0bb42e55a1ef8bf16cce5d5234
DevPool <heeren.bhanu@gmail.com>
2023-07-08 13:58:03 +0530

    Update index.js
    token request code test

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
-})
+});
+app.get("/agora-token", (req, res) => {
+    console.log("just got token request");
+    res.send('token request!');
+});
 app.listen(process.env.PORT || 3000)
